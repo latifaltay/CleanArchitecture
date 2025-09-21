d8151fd04231653c99213e5920555bbf93f2dd06
latifaltay <latifaltay00@gmail.com>
2025-09-21 05:05:30 +0300

    abstract ApiController has been created

---
+++ CleanArchitecture.Presentation/Controllers/CarsController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar;
+using CleanArchitecture.Presentation.Abstraction;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Controllers;
+
+public sealed class CarsController : ApiController
+{
+    public CarsController(IMediator mediator) : base(mediator)
+    {
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateCarCommand request, CancellationToken cancellationToken) 
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return Ok(response);
+    }
+
+}
